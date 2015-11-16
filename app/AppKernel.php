41ca1db879f7cece1563406d5fff0d1852d87172
Yu <eternitywisher@gmail.com>
2015-11-17 00:01:41 +1100

    Removed admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Dart\AppBundle\AppBundle(),
-            new Dart\AdminBundle\AdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
         );
 
