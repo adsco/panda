ef945acca9f79ce7c301de480aeba9f0d1eec711
Yu <eternitywisher@gmail.com>
2015-11-22 02:27:16 +1100

    Changed "Default" controller to Index
    Changed "Default" and Default view to Index

--- src/Dart/AppBundle/Controller/IndexController.php
+++ src/Dart/AppBundle/Controller/IndexController.php
@@ -4,10 +4,10 @@ namespace Dart\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        return $this->render('AppBundle:Index:index.html.twig');
     }
 }
