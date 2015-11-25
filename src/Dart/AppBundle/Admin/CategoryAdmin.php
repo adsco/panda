f0a42ae2f00a1061e348e6b5320244d0e9e12727
Yu <eternitywisher@gmail.com>
2015-11-26 05:07:12 +1100

    Added category entity
    Added category entity

---
+++ src/Dart/AppBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Dart\AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Dart\AppBundle\Entity\Category;
+
+/**
+ * Cateogry admin class
+ * 
+ * @package Dart\AppBundle
+ * @subpackage Admin
+ * @author Valerii Ten <eternitywisher@gmail.com>
+ */
+class CategoryAdmin extends Admin
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('cuisine', 'sonata_type_model', array(
+                'class' => 'Dart\AppBundle\Entity\Cuisine',
+                'property' => 'name',
+                'label' => 'Cuisine'
+            ))
+            ->add('name', 'text', array(
+                'label' => 'Name'
+            ))
+        ;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper
+            ->add('cuisine.name')
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper
+            ->addIdentifier('cuisine.name')
+            ->addIdentifier('name')
+        ;
+    }
+    
+    /**
+     * 
+     * @param Category $object
+     * @return type{@inheritDoc}
+     */
+    public function toString($object)
+    {
+        return $object instanceof Category ? $object->getName() : 'Category';
+    }
+}
