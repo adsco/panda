2abdbe01a12810c46835c646b587f5e895ddddee
Yu <eternitywisher@gmail.com>
2015-12-06 13:11:45 +1100

    Added groups

---
+++ src/Dart/AppBundle/Entity/Group.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dart\AppBundle\Entity;
+
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Group
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
