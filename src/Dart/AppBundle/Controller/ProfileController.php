f79b91eb8641861278b79635fadec28cce281d58
Yu <eternitywisher@gmail.com>
2015-12-11 17:59:48 +1100

    Added profile address and orders markups

--- src/Dart/AppBundle/Controller/ProfileController.php
+++ src/Dart/AppBundle/Controller/ProfileController.php
@@ -3,6 +3,9 @@
 namespace Dart\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Dart\AppBundle\Form\Type\DeliveryAddressType;
+use Dart\AppBundle\Entity\DeliveryAddress;
 
 /**
  * Profile controller
@@ -18,7 +21,74 @@ class ProfileController extends Controller
      */
     public function addressAction()
     {
-        return $this->render('AppBundle:Profile:address.html.twig');
+        $deliveryAddress = new DeliveryAddress();
+        $form = $this->createForm(new DeliveryAddressType(), $deliveryAddress);
+                
+        return $this->render('AppBundle:Profile:address.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+    
+    /**
+     * Add new address
+     */
+    public function addressAddAction(Request $request)
+    {
+        $deliveryAddress = new DeliveryAddress();
+        $form = $this->createForm(new DeliveryAddress(), $deliveryAddress);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($deliveryAddress);
+            $em->flush();
+        }
+        
+        return $this->redirectToRoute('profile_address');
+    }
+    
+    /**
+     * Edit existing address
+     */
+    public function addressEditAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $profile = $this->getUser()->getProfile();
+        $address = $em->getRepository('AppBundle:DeliveryAddress')->findOneBy(array(
+            'id' => $id,
+            'profile_id' => $profile->getId()
+        ));
+        
+        if (!$address) {
+            throw $this->createNotFoundException('Address not found');
+        }
+        
+        return $this->render('AppBundle:Profile:address_edit.html.twig', array(
+            'address' => $address
+        ));
+    }
+    
+    /**
+     * Remove existing address
+     */
+    public function addressRemoveAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $profile = $this->getUser()->getProfile();
+        $address = $em->getRepository('AppBundle:DeliveryAddress')->findOneBy(array(
+            'id' => $id,
+            'profile_id' => $profile->getId()
+        ));
+        
+        if (!$address) {
+            throw $this->createNotFoundException('Address not found');
+        }
+        
+        $em->remove($address);
+        $em->flush();
+        
+        return $this->redirectToRoute('profile_address');
     }
 
     /**
