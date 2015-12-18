a2ecf0a7edf3ed79baddedbc287bec9a2d8981d7
Yu <eternitywisher@gmail.com>
2015-12-18 23:41:19 +1100

    Added menu renderer

--- src/Dart/AppBundle/Controller/NavigationController.php
+++ src/Dart/AppBundle/Controller/NavigationController.php
@@ -4,16 +4,22 @@ namespace Dart\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Navigation Controller
+ * 
+ * @package \Dart\AppBundle
+ * @subpackage Controller
+ * @author Valerii Ten <eternitywisher@gmail.com>
+ */
 class NavigationController extends Controller
 {
-    public function CuisineNavigationAction($short = null)
+    public function menuAction()
     {
-        $em       = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
         $cuisines = $em->getRepository('AppBundle:Cuisine')->findAll();
         
-        return $this->render('AppBundle:Navigation:nav_cuisine.html.twig', array(
-            'cuisines' => $cuisines,
-            'dropdown' => $short
+        return $this->render('AppBundle:Navigation:nav_main.html.twig', array(
+            'cuisines' => $cuisines
         ));
     }
 }
